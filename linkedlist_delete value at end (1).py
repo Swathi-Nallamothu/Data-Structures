62fd7bdfcb30376f5dcdbba667154fc4c78200ff
Swathi Nallamothu <swathichinnarao1@gmail.com>
2025-07-24 16:14:16 +0530

    Add files via upload

---
+++ linkedlist_delete value at end (1).py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Node:
+    def __init__(s,data):
+        s.data=data
+        s.next= None
+class Linkedlist:
+    def __init__(s):
+        s.head= None
+    def i_a_b(s,data):
+        newnode=Node(data)
+        if not s.head:
+            s.head=newnode
+            return
+        current=s.head
+        while current.next:
+            current=current.next
+        current.next=newnode
+    def deleteEnd(s):
+        if s.head is None:
+            print("empty=ll")
+            return
+        elif s.head.next is None:
+            print(f" deleted last Node:{s.head.data}")
+            s.head=None
+            return
+        current=s.head
+        while current.next.next:
+            current=current.next
+        print(f"deleted last node:{current.next.data}")
+        current.next= None
+    def display(s):
+        current=s.head
+        if not current:
+            print("linked list is empty")
+            return
+        while current:
+            print(current.data,end="---")
+            current=current.next
+        print(None)
+        
+        
+l1=Linkedlist()
+while True:
+    print("\n Linkedlist- Insert at begin...")
+    print("1. Insert")
+    print("2. Display")
+    print("3. delete")
+    print("4. Exit")
+    choice=input("enter choice")
+    if choice=='1':
+        data=int(input("inserted value"))
+        l1.i_a_b(data)
+    elif choice=="2":
+        l1.display()
+    elif choice=="3":
+        l1.deleteEnd()
+    elif choice=="4":
+        print("exit the operation")
+        break
+    else:
+        print("invalid choice")
+
+        
+        
+
+
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
