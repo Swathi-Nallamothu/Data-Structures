90330106a684bd90d8aa43e83e9bbd56bb09614a
Swathi Nallamothu <swathichinnarao1@gmail.com>
2025-07-29 11:41:52 +0530

    Add files via upload

---
+++ LL-IAB-DBV.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Node:
+    def __init__(s,data):
+        s.data=data
+        s.next= None
+class Linkedlist:
+    def __init__(s):
+        s.head=None
+    def i_a_b(s,data):
+        newnode=Node(data)
+        newnode.next=s.head
+        s.head=newnode
+        print(f"{data} inserted from begin...")
+    def delete_by_value(s, value):
+        current = s.head
+        prev = None
+        if current is not None and current.data == value:
+            s.head = current.next
+            print("Deleted node with value:", value)
+            return
+        while current is not None and current.data != value:
+            prev = current
+            current = current.next
+        if current is None:
+            print("Value not found:", value)
+            return
+        prev.next = current.next
+        print("Deleted node with value:", value)
+
+    
+    def display(s):
+        current=s.head
+        if not current:
+            print("linked list is empty")
+            return
+        while current:
+            print(current.data,end="---")
+            current=current.next
+        print(None)
+        
+        
+l1=Linkedlist()
+while True:
+    print("\n Linkedlist- Insert at begin...")
+    print("1. Insert")
+    print("2. Display")
+    print("3.delete")
+    print("4. Exit")
+    choice=input("enter choice")
+    if choice=='1':
+        data=int(input("inserted value"))
+        l1.i_a_b(data)
+    elif choice=="2":
+        l1.display()
+    elif choice=="3":
+        value=int(input("value"))
+        l1.delete_by_value(value)
+    elif choice=="4":
+        print("exit the operation")
+        break
+    else:
+        print("invalid choice")
+
+
+# In[ ]:
+
+
+
+
